b169a517f852bd761a33746b34e63d6e369b7eb4
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2020-12-08 20:02:16 +0100

    Création d'une liste de test pour les Annonces

---
+++ front/src/app/mock-annonces.ts
@@ -0,0 +1,23 @@
+import { Annonce } from './annonce';
+
+export const Annonces: Annonce[] = [
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+    {id: 10, name: "je travaille", description: "j'utilise mon clavier pour écrire du code", host_since: new Date(), location: "chez moi"},
+];
