caa0c4b2da0b54b886f56f58e62bc17f9f9198f2
Clément Robin <clem27370@gmail.com>
2020-12-10 13:28:26 +0100

    initial commit

--- front/src/app/dropzone/dropzone.component.ts
+++ front/src/app/dropzone/dropzone.component.ts
@@ -9,7 +9,17 @@ export class DropzoneComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {   
   }
+  files: File[] = [];
 
+  onSelect(event) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+  
+  onRemove(event) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
 }
